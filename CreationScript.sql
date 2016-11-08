52d9d11ed24c82adc08c8b6eaba0fd714eac852d
Arthur Clark <arthur.clark@oit.edu>
2016-11-07 18:55:28 -0800

    Updated Data types and sizes.

--- CreationScript.sql
+++ CreationScript.sql
@@ -34,95 +34,95 @@ IF OBJECT_ID('Server', 'U') IS NOT NULL
 --Create Server Table
 CREATE TABLE [Server]
 	(ServerID			INT					PRIMARY KEY IDENTITY(1,1),
-	ServerName			VARCHAR(25)			NOT NULL,
+	ServerName			VARCHAR(75)			NOT NULL,
 	ServerIP			VARCHAR(15)			NOT NULL,	
-	ServerDomain		VARCHAR(50)			DEFAULT NULL,
-	SchoolName			VARCHAR(25)			NOT NULL,
-	AdminUsername		VARCHAR(30)			DEFAULT 'Admin',	
-	AdminPassword		VARCHAR(64)			DEFAULT 'Password',
-	EmailCredentials	NVARCHAR(50)		DEFAULT NULL)
+	ServerDomain			VARCHAR(50)			DEFAULT NULL,
+	SchoolName			VARCHAR(75)			NOT NULL,
+	AdminUsername			VARCHAR(30)			DEFAULT 'Admin',	
+	AdminPassword			BINARY(64)			DEFAULT 'Password',
+	EmailCredentials		NVARCHAR(50)			DEFAULT NULL)
 
 --Create Messages Table
 CREATE TABLE [Messages]
-	(MessageID			INT					PRIMARY KEY IDENTITY(1,1),
-	MessageData			NVARCHAR(500)		NOT NULL)
+	(MessageID			INT				PRIMARY KEY IDENTITY(1,1),
+	MessageData			NVARCHAR(500)			NOT NULL)
 
 --Create File Table
 CREATE TABLE Files
-	(FileID				INT					PRIMARY KEY IDENTITY(1,1),
-	FileData			VARBINARY(MAX) 		NOT NULL);
+	(FileID				INT				PRIMARY KEY IDENTITY(1,1),
+	FileData			VARBINARY(MAX) 			NOT NULL);
 
 --Create Chat Table
 CREATE TABLE Chat
-	(ChatID				INT					PRIMARY KEY IDENTITY(1,1),
-	MessageID			INT					DEFAULT NULL REFERENCES [Messages](MessageID),
-	FileID				INT					DEFAULT NULL REFERENCES Files(FileID))
+	(ChatID				INT				PRIMARY KEY IDENTITY(1,1),
+	MessageID			INT				DEFAULT NULL REFERENCES [Messages](MessageID),
+	FileID				INT				DEFAULT NULL REFERENCES Files(FileID))
 		
 --Create Official Mentor Table
 CREATE TABLE OfficialMentor
-	(MentorID			INT					PRIMARY KEY IDENTITY(1,1),
-	OrganizationName	NVARCHAR(50)		NOT NULL)
+	(MentorID			INT				PRIMARY KEY IDENTITY(1,1),
+	OrganizationName		NVARCHAR(50)			NOT NULL)
 
 --Create UserProfile Table
 CREATE TABLE UserProfile
-	(UserID				INT					PRIMARY KEY IDENTITY(1,1),
-	DisplayFName		VARCHAR(30)			NOT NULL,
-	DisplayLName		VARCHAR(30)			NOT NULL,
-	EmailAddress		VARCHAR(50)			NOT	NULL,
-	UserPassword		BINARY(64)			NOT NULL,
+	(UserID				INT				PRIMARY KEY IDENTITY(1,1),
+	DisplayFName			VARCHAR(30)			NOT NULL,
+	DisplayLName			VARCHAR(30)			NOT NULL,
+	EmailAddress			VARCHAR(50)			NOT	NULL,
+	UserPassword			BINARY(64)			NOT NULL,
 	Privileges			BINARY(4)			NOT NULL)
 
 --Create Official Mentor Table
 CREATE TABLE OmToUser
-	(UserID				INT					NOT NULL	REFERENCES UserProfile(UserID),
-	MentorID			INT					NOT NULL	REFERENCES OfficialMentor(MentorID)
+	(UserID				INT				NOT NULL	REFERENCES UserProfile(UserID),
+	MentorID			INT				NOT NULL	REFERENCES OfficialMentor(MentorID)
 	PRIMARY KEY(UserID, MentorID))
 
 --Create UserToChat Table
 CREATE TABLE UserToChat
-	(UserID				INT					NOT NULL	REFERENCES UserProfile(UserID),
-	 ChatID				INT					NOT NULL	REFERENCES Chat(ChatID)
+	(UserID				INT				NOT NULL	REFERENCES UserProfile(UserID),
+	 ChatID				INT				NOT NULL	REFERENCES Chat(ChatID)
 	PRIMARY KEY (UserID, ChatID))
 
 --Create Classes Table
 CREATE TABLE Picture
-	(UserID				INT					NOT NULL	REFERENCES UserProfile(UserID),
-	Photo				VARBINARY(MAX)		NULL)	
+	(UserID				INT				NOT NULL	REFERENCES UserProfile(UserID),
+	Photo				VARBINARY(MAX)			NULL)	
 
 --Create Classes Table
 CREATE TABLE Classes
-	(ClassID			INT					PRIMARY KEY IDENTITY(1,1),
+	(ClassID			INT				PRIMARY KEY IDENTITY(1,1),
 	CourseName			VARCHAR(50)			NOT NULL,	--Common Name
 	CourseCode			VARCHAR(5)			NOT NULL,	--Ex. WRI
-	CourseNumber		SMALLINT			NOT NULL,	--Ex. 121
+	CourseNumber			SMALLINT			NOT NULL,	--Ex. 121
 	TermOffered			TINYINT				NOT NULL)
 
 --Create User To Class Table
 CREATE TABLE UserToClass
-	(UserID				INT					NOT NULL	REFERENCES UserProfile(UserID),
-	ClassID				INT					NOT NULL	REFERENCES Classes(ClassID)
+	(UserID				INT				NOT NULL	REFERENCES UserProfile(UserID),
+	ClassID				INT				NOT NULL	REFERENCES Classes(ClassID)
 	PRIMARY KEY (UserID, ClassID))
 
 --Create Report Table
 CREATE TABLE Report
-	(ReportID			INT					PRIMARY KEY IDENTITY(1,1),
-	 FlaggerID			INT					NOT NULL	REFERENCES UserProfile(UserID))
+	(ReportID			INT				PRIMARY KEY IDENTITY(1,1),
+	 FlaggerID			INT				NOT NULL	REFERENCES UserProfile(UserID))
 
 --Create Review Table
 CREATE TABLE Review
-	(ReviewID			INT					PRIMARY KEY IDENTITY(1,1),
-	ReportID			INT					NOT NULL	REFERENCES Report(ReportID),
-	StarRanking			TINYINT					NOT NULL,
-	[Description]		NVARCHAR(250)		NULL)
+	(ReviewID			INT				PRIMARY KEY IDENTITY(1,1),
+	ReportID			INT				NOT NULL	REFERENCES Report(ReportID),
+	StarRanking			TINYINT				NOT NULL,
+	[Description]			NVARCHAR(250)			NULL)
 
 --Create Sticker Table
 CREATE TABLE Sticker
-	(StickerID			INT					PRIMARY KEY IDENTITY(1,1),
-	ProblemDescription	NVARCHAR(500)		NOT NULL,
-	ClassID				INT					NOT NULL	REFERENCES Classes(ClassID),
-	StudentID			INT					NOT NULL	REFERENCES UserProfile(UserID),
-	TutorID				INT					NOT NULL	REFERENCES UserProfile(UserID),
-	StudentReviewID		INT					NULL		REFERENCES Review(ReviewID),
-	TutorReviewID		INT					NULL		REFERENCES Review(ReviewID),
+	(StickerID			INT				PRIMARY KEY IDENTITY(1,1),
+	ProblemDescription		NVARCHAR(500)			NOT NULL,
+	ClassID				INT				NOT NULL	REFERENCES Classes(ClassID),
+	StudentID			INT				NOT NULL	REFERENCES UserProfile(UserID),
+	TutorID				INT				NOT NULL	REFERENCES UserProfile(UserID),
+	StudentReviewID			INT				NULL		REFERENCES Review(ReviewID),
+	TutorReviewID			INT				NULL		REFERENCES Review(ReviewID),
 	SubmitTime			DATETIME			NOT NULL,
 	[Timeout]			DATETIME2			NOT NULL)
