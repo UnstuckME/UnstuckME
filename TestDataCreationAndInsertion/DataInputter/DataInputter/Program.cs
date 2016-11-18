e9e2beaab704604cf6878a306e42a412bd831744
Unknown <kyronnr@gmail.com>
2016-11-18 14:48:59 -0800

    Adeed Data Creation and Insertion Programs

---
+++ TestDataCreationAndInsertion/DataInputter/DataInputter/Program.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    WARNING : This code has no error checking
+    
+    Notes: you will need to update the connection strings to contain your user
+    name and your password. also the csv files are located using an absolute file path
+    that you will need to update to refelct the correct location of the files
+    on your pc.
+
+    Added Info on running this program:
+    run the DB recreation scripts before running this code.
+    This code will take 15-20 minutes to complete its opperations.
+*/
+
+
+
+
+namespace DataInputter
+{
+   public  class DataHolderNames
+    {
+        public String ID;
+        public String Fname;
+        public String Lname;
+        public String Email;
+        public String Password;
+        public String Privelidges;
+    }
+
+    public class DataHolderClasses
+    {
+        public String ID;
+        public String CourseName;
+        public String Code;
+        public String Number;
+        public String Term;
+    }
+
+    public class DataHolderStickers
+    {
+        public String ID;
+        public String ProblemDescription;
+        public String ClassID;
+        public String StudentID;
+        public String TutorID;
+        public String StudentReviewID;
+        public String TutorReviewID;
+        public String MinimumStarRanking;
+        public String SubmitTime;
+        public String Timeout;
+    }
+
+    public class DataHolderReviews
+    {
+        public String ID;
+        public String StarRanking;
+        public String Description;
+        public String StickerID;
+        public String UserID;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Password;
+            string UserName;
+            
+            Console.WriteLine("Enter User Name:");
+            UserName = Console.ReadLine();
+            Console.WriteLine("EnterPassword:");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Password = Console.ReadLine();
+            string connectionString = "Persist Security Info=False;User ID=" + UserName + ";" + "Password=" + Password +
+                ";Initial Catalog=UnStuckME_DB;Server=aura.students.cset.oit.edu";
+            Console.Clear();
+            Console.ResetColor();
+
+            Console.WriteLine("Begining Inserting Users");
+            InsertUsers(connectionString);
+            Console.WriteLine("         Inserting Users Complete");
+            Console.WriteLine("Begining Inserting Classes");
+            InsertClasses(connectionString);
+            Console.WriteLine("         Inserting Classes Complete");
+            Console.WriteLine("Begining Inserting Stickers");
+            InsertStickers(connectionString);
+            Console.WriteLine("         Inserting Stickers Complete");
+            Console.WriteLine("Begining Inserting Reviews");
+            InsertReviews(connectionString);
+            Console.WriteLine("         Inserting Reviews Complete");
+            Console.WriteLine("Done");
+            
+        }
+        static void InsertUsers(string connectionString)
+        {
+            //update the file path in each insert method
+            StreamReader reader = new StreamReader(File.OpenRead(@"..\..\..\..\FakeDataUsers.csv"));
+            List<DataHolderNames> listA = new List<DataHolderNames>();
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (!String.IsNullOrWhiteSpace(line))
+                {
+                    string[] values = line.Split(',');
+                    if (values.Length >= 6)
+                    {
+                        DataHolderNames cont = new DataHolderNames();
+                        cont.ID = values[0];
+                        cont.Fname = values[1];
+                        cont.Lname = values[2];
+                        cont.Email = values[3];
+                        cont.Password = values[4];
+                        cont.Privelidges = values[5];
+
+                        listA.Add(cont);
+                    }
+                }
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                foreach (DataHolderNames item in listA)
+                {
+                    SqlCommand cmd = new SqlCommand("INSERT INTO UserProfile (DisplayFName, DisplayLName, EmailAddress, UserPassword, Privileges) VALUES (@DisplayFName, @DisplayLName, @EmailAddress, @UserPassword, @Privileges)");
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = connection;
+                    cmd.Parameters.AddWithValue("@DisplayFName", item.Fname);
+                    cmd.Parameters.AddWithValue("@DisplayLName", item.Lname);
+                    cmd.Parameters.AddWithValue("@EmailAddress", item.Email);
+                    cmd.Parameters.AddWithValue("@UserPassword", item.Password);
+                    cmd.Parameters.AddWithValue("@Privileges", item.Privelidges);
+
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+
+        static void InsertClasses(string connectionString)
+        {
+            StreamReader reader = new StreamReader(File.OpenRead(@"..\..\..\..\FakeDataClasses.csv"));
+            List<DataHolderClasses> listA = new List<DataHolderClasses>();
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (!String.IsNullOrWhiteSpace(line))
+                {
+                    string[] values = line.Split(',');
+                    if (values.Length >= 5)
+                    {
+                        DataHolderClasses cont = new DataHolderClasses();
+                        cont.ID = values[0];
+                        cont.CourseName = values[1];
+                        cont.Code = values[2];
+                        cont.Number = values[3];
+                        cont.Term = values[4];
+                        
+                        listA.Add(cont);
+                    }
+                }
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                foreach (DataHolderClasses item in listA)
+                {
+                    SqlCommand cmd = new SqlCommand("INSERT INTO Classes (CourseName, CourseCode, CourseNumber, TermOffered) VALUES (@CourseName, @CourseCode, @CourseNumber, @TermOffered)");
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = connection;
+                    cmd.Parameters.AddWithValue("@CourseName", item.CourseName);
+                    cmd.Parameters.AddWithValue("@CourseCode", item.Code);
+                    cmd.Parameters.AddWithValue("@CourseNumber", Convert.ToInt16(item.Number));
+                    cmd.Parameters.AddWithValue("@TermOffered", 2);
+                    
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+
+        static void InsertStickers(string connectionString)
+        {
+            StreamReader reader = new StreamReader(File.OpenRead(@"..\..\..\..\FakeDataStickers.csv"));
+            List<DataHolderStickers> listA = new List<DataHolderStickers>();
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (!String.IsNullOrWhiteSpace(line))
+                {
+                    string[] values = line.Split(',');
+                    if (values.Length >= 7)
+                    {
+                        DataHolderStickers cont = new DataHolderStickers();
+                        cont.ID = values[0];
+                        cont.ClassID = values[1];
+                        cont.ProblemDescription = values[2];
+                        cont.StudentID = values[3];
+                        cont.TutorID= values[4];
+                        //cont.StudentReviewID = values[5];
+                        //cont.TutorReviewID = values[6];
+                        //cont.MinimumStarRanking = values[7];
+                        //cont.Timeout = values[8];
+                        //cont.SubmitTime = values[9];
+
+
+                        listA.Add(cont);
+                    }
+                }
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                foreach (DataHolderStickers item in listA)
+                {
+                    SqlCommand cmd = new SqlCommand("INSERT INTO Sticker (ProblemDescription, ClassID, StudentID, TutorID, MinimumStarRanking, SubmitTime, Timeout) VALUES (@ProblemDescription, @ClassID, @StudentID, @TutorID, @MinimumStarRanking, @SubmitTime, @Timeout)");
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = connection;
+                    cmd.Parameters.AddWithValue("@ProblemDescription", item.ProblemDescription);
+                    cmd.Parameters.AddWithValue("@ClassID", Convert.ToInt32(item.ClassID));
+                    cmd.Parameters.AddWithValue("@StudentID", Convert.ToInt32(item.StudentID));
+                    cmd.Parameters.AddWithValue("@TutorID", Convert.ToInt32(item.TutorID));
+                    cmd.Parameters.AddWithValue("@MinimumStarRanking", .5);
+                    cmd.Parameters.AddWithValue("@SubmitTime", DateTime.Now);
+                    cmd.Parameters.AddWithValue("@Timeout", DateTime.MaxValue);
+
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+        static void InsertReviews(string connectionString)
+        {
+            StreamReader reader = new StreamReader(File.OpenRead(@"..\..\..\..\FakeDataReviews.csv"));
+            List<DataHolderReviews> listA = new List<DataHolderReviews>();
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (!String.IsNullOrWhiteSpace(line))
+                {
+                    string[] values = line.Split(',');
+                    if (values.Length >= 4)
+                    {
+                        DataHolderReviews cont = new DataHolderReviews();
+                        cont.ID = values[0];
+                        cont.StickerID = values[1];
+                        cont.UserID = values[2];
+                        cont.StarRanking = values[3];
+                        cont.Description = values[4];
+                        listA.Add(cont);
+                    }
+                }
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                foreach (DataHolderReviews item in listA)
+                {
+                    SqlCommand cmd = new SqlCommand("INSERT INTO Review (StickerID, ReviewerID, StarRanking, Description) VALUES (@StickerID, @ReviewerID, @StarRanking, @Description)");
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = connection;
+                    cmd.Parameters.AddWithValue("@StickerID", item.StickerID);
+                    cmd.Parameters.AddWithValue("@ReviewerID", item.UserID);
+                    cmd.Parameters.AddWithValue("@StarRanking", Convert.ToDouble(item.StarRanking));
+                    cmd.Parameters.AddWithValue("@Description", item.Description);
+                    
+
+
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+
+                    //if ((Convert.ToInt32(item.ID) % 2) == 1)
+                    //{
+                    //    cmd = new SqlCommand("UpdateStudentReviewIDByStudentReviewIDAndStickerID");
+                    //    cmd.Connection = connection;
+                    //    cmd.CommandType = CommandType.StoredProcedure;
+                    //    cmd.Parameters.Add(new SqlParameter("@StudentReviewID", Convert.ToInt32(item.ID)));
+                    //    cmd.Parameters.Add(new SqlParameter("@StickerID", Convert.ToInt32(item.StickerID)));
+                    //}
+                    //else
+                    //{
+                    //    cmd = new SqlCommand("UpdateTutorReviewIDByTutorReviewIDAndStickerID");
+                    //    cmd.Connection = connection;
+                    //    cmd.CommandType = CommandType.StoredProcedure;
+                    //    cmd.Parameters.Add(new SqlParameter("@TutorReviewID", Convert.ToInt32(item.ID)));
+                    //    cmd.Parameters.Add(new SqlParameter("@StickerID", Convert.ToInt32(item.StickerID)));
+                    //}
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+
+        static void InsertOfficialMentoringOrgs(string connectionString)
+        {
+
+        }
+        static void InsertOfficalMentors(string connectionString)
+        {
+            //InsertUserIntoMentorProgram
+        }
+        static void InsertChat(string connectionString)
+        {
+            //CreateChat
+            //InsertUserIntoChat
+        }
+        static void InsertMessages(string connectionString)
+        {
+
+        }
+    }
+}
---
+++ TestDataCreationAndInsertion/TestDataCreator/TestDataCreator/MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestDataCreator
+{
+
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double CreateStars(int WholePortion)
+        {
+            double star = 0;
+            switch (WholePortion)
+            {
+                case 1:
+                    star = .5;
+                    break;
+                case 2:
+                    star = 1;
+                    break;
+                case 3:
+                    star = 1.5;
+                    break;
+                case 4:
+                    star = 2;
+                    break;
+                case 5:
+                    star = 2.5;
+                    break;
+                case 6:
+                    star = 3;
+                    break;
+                case 7:
+                    star = 3.5;
+                    break;
+                case 8:
+                    star = 4;
+                    break;
+                case 9:
+                    star = 4.5;
+                    break;
+                case 10:
+                    star = 5;
+                    break;
+                default:
+                    break;
+            }
+            return star;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            button.Visibility = Visibility.Hidden;
+            // Sticker related numbers
+            int StartNum = Convert.ToInt32(TextIn_StickerStartNum.Text);       // the sticker start number
+            int NumToMake = Convert.ToInt32(TextIn_NumToCreate.Text);      // the number of new stickers you want to make
+            int CurrentNum = StartNum;     // the sticker number that is currently being created
+            int EndNum = StartNum + NumToMake;         // the last sticker to make
+
+            int MinClassNumber = Convert.ToInt32(TextIn_MinClassNum.Text);
+            int MaxClassNumber = Convert.ToInt32(TextIn_MaxClassNum.Text);
+
+            int MinUserNumber = Convert.ToInt32(TextIn_MinUserNum.Text);
+            int MaxUserNumber = Convert.ToInt32(TextIn_MaxUserNum.Text);
+
+            int MinUnstuckifier = Convert.ToInt32(TextIn_MinUnstucker.Text);
+            int MaxUnstuckifier = Convert.ToInt32(TextIn_MaxUnstucker.Text);
+
+            //review stuff
+            int ReviewStartNumber = Convert.ToInt32(TextIn_ReviewStartNumber.Text);
+
+            double MinStarUnstuckifier = Convert.ToDouble(comboBox_LowestStarUnstucker.Text);
+            double MaxStarUserUnstuckifier = Convert.ToDouble(comboBox_HighestStarUnstucker.Text);
+
+            double MinStarUser = Convert.ToDouble(comboBox_LowestStarUser.Text);
+            double MaxStarUser = Convert.ToDouble(comboBox_HighestStarUser.Text);
+            // random number makers
+            Random RandClass = new Random();
+
+            int MessageID = 1;
+            while(CurrentNum <= EndNum)
+            {
+                int ID = CurrentNum;
+                int ClassId = RandClass.Next(MinClassNumber, MaxClassNumber + 1);
+                String ProblemDesc = ("Test problem for class " + ClassId + " on Sticker " + ID);
+                int Reporter = RandClass.Next(MinUserNumber, MaxUserNumber + 1);
+                int Unstucker = RandClass.Next(MinUnstuckifier, MaxUnstuckifier + 1);
+                int ReviewIDStudent = ReviewStartNumber++;
+                int IDStudent = ReviewIDStudent;
+                String StudentReviewDesc = ("Test Review Number " + IDStudent + " for the help requesting student " + Reporter + " on sticker " + ID);
+                double StudentStars = CreateStars(RandClass.Next(1, 11));
+
+                int ReviewIDMentor = ReviewStartNumber++;
+                int IDMentor = ReviewIDMentor;
+                String MentorReviewDesc = ("Test Review Number " + IDMentor + " for the Mentor " + Unstucker + " on sticker " + ID);
+                double MentorStars = CreateStars(RandClass.Next(1, 11));
+
+                String StickerOutput = (ID + "," + ClassId + "," + ProblemDesc + "," + Reporter + "," + Unstucker);
+
+                String StudentReviewOutput = (IDStudent + "," + ID + "," + Reporter + "," + StudentStars + "," + StudentReviewDesc  );
+                String MentorReviewOutput = (IDMentor + "," + ID + "," + Unstucker + "," + MentorStars + "," + MentorReviewDesc );
+
+                String UserToChatStudentOutput = (Reporter + "," + CurrentNum);
+                String UserToChatMentorOutput = (Unstucker + "," + CurrentNum);
+
+
+                using (System.IO.StreamWriter StickerFile =
+                new System.IO.StreamWriter(@"..\..\..\..\Stickers.csv", true))
+                {
+                    StickerFile.WriteLine(StickerOutput);
+                }
+                using (System.IO.StreamWriter ReviewFile =
+                new System.IO.StreamWriter(@"..\..\..\..\Review.csv", true))
+                {
+                    ReviewFile.WriteLine(StudentReviewOutput);
+                    ReviewFile.WriteLine(MentorReviewOutput);
+                }
+                using (System.IO.StreamWriter ChatFile =
+                new System.IO.StreamWriter(@"..\..\..\..\Chat.csv", true))
+                {
+                    ChatFile.WriteLine(CurrentNum);
+                }
+                using (System.IO.StreamWriter UserToChatFile =
+                new System.IO.StreamWriter(@"..\..\..\..\UserToChat.csv", true))
+                {
+                    UserToChatFile.WriteLine(UserToChatStudentOutput);
+                    UserToChatFile.WriteLine(UserToChatMentorOutput);
+                }
+
+                int randMessageNumber = RandClass.Next(5, 30);
+                for (int i = 0; i < randMessageNumber; i++)
+                {
+                    String MessageOutput = (MessageID + "," + CurrentNum + "," + "Message " + i);
+                    using (System.IO.StreamWriter MessageFile =
+                    new System.IO.StreamWriter(@"..\..\..\..\Messages.csv", true))
+                    {
+                        MessageFile.WriteLine(MessageOutput);
+                    }
+                    MessageID++;
+                }
+
+
+
+
+                CurrentNum++;
+            }
+           button.Visibility = Visibility.Visible;
+        }
+    }
+}
