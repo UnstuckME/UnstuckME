fe94f9cc1fe043550ea05fc61aabe680b89cc698
Clarkmeister <arthur.clark@oit.edu>
2016-11-29 22:17:21 -0800

    Added Exception Handling for Server Login

--- UnstuckME/UnstuckMEServerGUI/ServerLogin.xaml.cs
+++ UnstuckME/UnstuckMEServerGUI/ServerLogin.xaml.cs
@@ -27,11 +27,41 @@ namespace UnstuckMEServerGUI
 
         private void buttonServerLogin_Click(object sender, RoutedEventArgs e)
         {
-            //SqlConnection conn = new SqlConnection("Server=aura.students.cset.oit.edu;Database=UnstuckME_DB;")
-            MainWindow mainWindow = new MainWindow();
-            App.Current.MainWindow = mainWindow;
-            this.Close();
-            mainWindow.Show();
+            try
+            {
+                SqlConnection conn = new SqlConnection("Server=aura.students.cset.oit.edu;Database=UnstuckME_DB;persist security info=True;user id=UnstuckME_Admin;password=B3$$t-P@$$W0rd");
+                conn.Open();
+
+                //Beginnings of username check
+                if (System.Data.ConnectionState.Open == conn.State)
+                {
+                    List<string> userNames = new List<string>();
+                    List<string> password = new List<string>();
+                    SqlCommand cmd = new SqlCommand("dbo.GetAdminInfo", conn);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        userNames.Add(rdr["AdminUsername"].ToString());
+                        password.Add(rdr["AdminPassword"].ToString());
+                    }
+
+                    conn.Close();
+                    MainWindow mainWindow = new MainWindow();
+                    App.Current.MainWindow = mainWindow;
+                    this.Close();
+                    mainWindow.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Connection Failed!", "Connection Failure", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    conn.Close();
+                }
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Connection Failure", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
         }
     }
 }
