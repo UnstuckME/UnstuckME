1b2467e8f4c0fce620bf3a95020bd5b11b8800fd
Matthew Cole <colematthew4@charter.net>
2017-01-12 22:02:51 -0800

    Some User Profile functionality implemented
    Change username
    Change password
    Delete account

--- UnstuckME/UnstuckMEUserGUI/UnstuckMEUserGUI/MainPage.xaml.cs
+++ UnstuckME/UnstuckMEUserGUI/UnstuckMEUserGUI/MainPage.xaml.cs
@@ -30,20 +30,18 @@ namespace UnstuckMEUserGUI
 		public MainPage(int UserID, IUnstuckMEService OpenServer)
 		{
             ImageSourceConverter ic = new ImageSourceConverter();
-            ImageSource img = null;
-            byte[] imgByte;
+
             //Opens a connection to UnstuckME Server.
             Server = OpenServer;
 			User = Server.GetUserInfo(UserID);
 			InitializeComponent();
-			FNameTxtBx.Text = User.FirstName; // get the name from the server and insert it
+			FNameTxtBx.Text = User.FirstName; // get the name from the server and show it
 			LNameTxtBx.Text = User.LastName;
 			EmailtextBlock.Text = User.EmailAddress; // get the email and show it
-            imgByte = Server.GetProfilePicture(UserID);
-            img = ic.ConvertFrom(imgByte) as ImageSource;
-            UserPhoto.Source = img;
+			byte[] imgByte = Server.GetProfilePicture(UserID);
+			UserPhoto.Source = ic.ConvertFrom(imgByte) as ImageSource;	//convert image so it can be displayed
 
-            for (int i = 0; i < 50; i++)
+            for (int i = 0; i < 50; i++)	//just testing
 			{
 				TextBlock test = new TextBlock();
 				test.Text = "test text" + i;
@@ -51,6 +49,7 @@ namespace UnstuckMEUserGUI
 			}
 		}
 
+		//Adds/removes classes from the list of classes user can mentor for
 		private void AddRemoveClasses_Click(object sender, RoutedEventArgs e)
 		{
 			if (AddRemoveClassesView.Visibility == Visibility.Collapsed)
@@ -67,12 +66,14 @@ namespace UnstuckMEUserGUI
 
 		}
 
+		//Commits changes to the user's classes to mentor
 		private void Commit_Click(object sender, RoutedEventArgs e)
 		{
 			ClassesView.Visibility = Visibility.Visible;
 			AddRemoveClassesView.Visibility = Visibility.Collapsed;
 		}
 
+		//Uploads a new photo from the user's computer and inserts it as the user's new profile picture
 		private void UserPhotoBtn_Click(object sender, RoutedEventArgs e)
 		{
 			OpenFileDialog file_browser = new OpenFileDialog();
@@ -80,32 +81,79 @@ namespace UnstuckMEUserGUI
 			file_browser.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
 			file_browser.Multiselect = false;
 			file_browser.ValidateNames = true;
-			file_browser.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg";
+			file_browser.Filter = "Image Files (*.jpeg;*.png;*.jpg)|*.jpeg;*.png;*.jpg|JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg";
 
 			if (file_browser.ShowDialog() == DialogResult.OK)
 			{
-				Stream file = file_browser.OpenFile();
-				byte[] image = null;
+				//FileStream file = new FileStream(file_browser.FileName, FileMode.Open, FileAccess.Read);
+				////FileStream file = file_browser.OpenFile() as FileStream;
+				//int stream_length = (int)file.Length;
+				//byte[] byte_array = new byte[stream_length];
+				//file.Read(byte_array, 0, stream_length);
+				//file.Close();
+
+				/********************************************************************************
+				System.Drawing.Image image = System.Drawing.Image.FromStream(file);
 
 				using (MemoryStream ms = new MemoryStream())
 				{
-					file.CopyTo(ms);
-					image = ms.ToArray();
-				}
+					if (file_browser.SafeFileName.Split('.').Last() == "jpeg" || file_browser.SafeFileName.Split('.').Last() == "jpg")
+						image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+					else if (file_browser.SafeFileName.Split('.').Last() == "png")
+						image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
 
-				Server.SetProfilePicture(User.UserID, image);
-                ImageSourceConverter ic = new ImageSourceConverter();
-                ImageSource img = null;
-                byte[] imgByte;
-                imgByte = Server.GetProfilePicture(User.UserID);
-                img = ic.ConvertFrom(imgByte) as ImageSource;
-                UserPhoto.Source = img;
-            }
+					byte_array = ms.ToArray();
+				}
+				********************************************************************************/
+
+				//Server.SetProfilePicture(User.UserID, byte_array);
+
+				//ImageSourceConverter ic = new ImageSourceConverter();
+				//UserPhoto.Source = ic.ConvertFrom(byte_array) as ImageSource;
+			}
 		}
 
+		//Opens prompt for user to change their username
 		private void ChangeUserName_Click(object sender, RoutedEventArgs e)
 		{
-			//Server.ChangeUserName(User.EmailAddress, FNameTxtBx.Text, LNameTxtBx.Text);
+			FNameTxtBx.Text = string.Empty;
+			LNameTxtBx.Text = string.Empty;
+			ChangeUsernameBtn.Visibility = Visibility.Visible;
+			AllowChangePasswordBtn.Visibility = Visibility.Hidden;
+		}
+
+		//Submits the username changes
+		private void ChangeUsernameBtn_Click(object sender, RoutedEventArgs e)
+		{
+			Server.ChangeUserName(User.EmailAddress, FNameTxtBx.Text, LNameTxtBx.Text);
+			ChangeUsernameBtn.Visibility = Visibility.Hidden;
+			AllowChangePasswordBtn.Visibility = Visibility.Visible;
+		}
+
+		//Submits the password changes
+		private void ChangePasswordBtn_Click(object sender, RoutedEventArgs e)
+		{
+			Server.ChangePassword(User, ChangePasswordBx.Text);
+			ChangePasswordBx.Visibility = Visibility.Hidden;
+			ChangePasswordBx.Text = string.Empty;
+			ChangePasswordBtn.Visibility = Visibility.Hidden;
+			AllowChangePasswordBtn.Visibility = Visibility.Visible;
+		}
+
+		//Opens prompt for user to change their password
+		private void AllowChangePasswordBtn_Click(object sender, RoutedEventArgs e)
+		{
+			ChangePasswordBx.Visibility = Visibility.Visible;
+			ChangePasswordBtn.Visibility = Visibility.Visible;
+			AllowChangePasswordBtn.Visibility = Visibility.Hidden;
+		}
+		
+		//Deletes the account and logs out, goes to login page
+		private void DeleteAccountBtn_Click(object sender, RoutedEventArgs e)
+		{
+			Server.Logout();
+			Server.DeleteUserAccount(User.UserID);
+			NavigationService.Navigate(new LoginPage(Server));
 		}
 	}
 }
