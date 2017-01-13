4580b05c582b5b00211703b8c57c06c06c0795e2
Clarkmeister <arthur.clark@oit.edu>
2017-01-12 21:05:04 -0800

    Admin Online Users List Working

--- UnstuckME/UnstuckMEServerGUI/ServerCallback.cs
+++ UnstuckME/UnstuckMEServerGUI/ServerCallback.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using UnstuckMEInterfaces;
 
 namespace UnstuckMEServerGUI
@@ -19,6 +20,23 @@ namespace UnstuckMEServerGUI
         public bool isOnline()
         {
             return true;
+        }
+
+        public void GetUpdate(int value , string emailAddress)
+        {
+            switch(value)
+            {
+                case 0:
+                    {
+                        ((ServerRunning)Application.Current.MainWindow).AddUser(emailAddress);
+                        break;
+                    }
+                case 1:
+                    {
+                        ((ServerRunning)Application.Current.MainWindow).RemoveUser(emailAddress);
+                        break;
+                    }
+            }
         }
     }
 }
