a11ea60d766bc728c0378970ee1c54cf2eb92d12
Clarkmeister <arthur.clark@oit.edu>
2017-01-26 21:56:42 -0800

    Duplicate ServerGUI Applications Not Allowed

--- UnstuckME/UnstuckMEServerGUI/App.xaml.cs
+++ UnstuckME/UnstuckMEServerGUI/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,18 @@ namespace UnstuckMEServerGUI
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (Process.GetProcessesByName(System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location)).Count() > 1)
+            {
+                foreach (Process process in Process.GetProcessesByName(System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location)))
+                {
+                    if (process != Process.GetCurrentProcess())
+                    {
+                        process.CloseMainWindow();
+                    }
+                }
+            }
+        }
     }
 }
