efbb5a59111cfc4f16a7c408dad7765e5b310b35
Clarkmeister <arthur.clark@oit.edu>
2017-01-26 22:44:20 -0800

    Added some error reporting to program.cs

--- UnstuckME/UnstuckMEServer/Program.cs
+++ UnstuckME/UnstuckMEServer/Program.cs
@@ -13,19 +13,24 @@ namespace UnstuckMEInterfaces
         public static UnstuckMEService _server;
         static void Main(string[] args)
         {
-            /*
-             *Opens an UnstuckMEService for clients to connect to.  
-             */
-             //testiung things
-            _server = new UnstuckMEService();
-            using (ServiceHost host = new ServiceHost(_server))
+            try
             {
-                host.Open();
-                //Task.Factory.StartNew(_server.CheckStatus, CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default);
-                Thread userStatusCheck = new Thread(_server.CheckUserStatus);
-                userStatusCheck.Start();
-                Console.WriteLine("Server is Running...");
-                Console.WriteLine("<Press Enter to Shut Down Server");
+                _server = new UnstuckMEService();
+                using (ServiceHost host = new ServiceHost(_server))
+                {
+
+                    host.Open();
+                    Thread userStatusCheck = new Thread(_server.CheckUserStatus);
+                    userStatusCheck.Start();
+                    Console.WriteLine("Server is Running...");
+                    Console.WriteLine("<Press Enter to Shut Down Server>");
+                    Console.ReadLine();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("\nSERVER START ERROR: \n" + ex.Message);
+                Console.WriteLine("\nPress Enter To Exit...");
                 Console.ReadLine();
             }
         }
