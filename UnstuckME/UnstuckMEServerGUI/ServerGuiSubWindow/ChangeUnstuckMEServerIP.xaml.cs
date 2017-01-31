f5065af467fabddb84f5e3e77ac791c30e0a5b89
RyanWBell <Ryan.Bell@oit.edu>
2017-01-31 01:09:26 -0800

    Changed Some connection String Shananigans that wern't getting set correctly
    - Changed School Database
    - Added preloading if they already have database configured else where
    - Add additional config options
    - WOrked on ServerGUI General Layout (Fixed some buttons)

---
+++ UnstuckME/UnstuckMEServerGUI/ServerGuiSubWindow/ChangeUnstuckMEServerIP.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UnstuckMEServerGUI.ServerGuiSubWindow
+{
+    /// <summary>
+    /// Interaction logic for ChangeUnstuckMEServerIP.xaml
+    /// </summary>
+    public partial class ChangeUnstuckMEServerIP : Window
+    {
+        public ChangeUnstuckMEServerIP(int schoolID)
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonTest_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
