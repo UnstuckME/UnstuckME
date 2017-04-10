89dde56bfc23ae90a31483161666ef0e4a847030
Matthew Cole <colematthew4@charter.net>
2017-04-10 12:46:25 -0700

    Added an icon for the windows

--- UnstuckME/UnstuckMEUserGUI/UnstuckMEUserGUI/Properties/Resources.Designer.cs
+++ UnstuckME/UnstuckMEUserGUI/UnstuckMEUserGUI/Properties/Resources.Designer.cs
@@ -121,6 +121,16 @@ namespace UnstuckMEUserGUI.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon UnstuckME_Application_Icon {
+            get {
+                object obj = ResourceManager.GetObject("UnstuckME_Application_Icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap UnstuckME_Logo {
